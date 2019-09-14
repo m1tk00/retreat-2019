4a2c54d88cf04b2932a194796795b6a0a198cb57
m1tk00 <mitko.kockovski@gmail.com>
2019-09-14 12:20:32 +0300

    Step 1

---
+++ retreat-ajaxing-woo/js/my_query.js
@@ -0,0 +1,18 @@
+(function($){
+	$( document ).ready(
+		function(){
+			$.ajax(
+				{
+					url: ajax_object.ajax_url,
+					data: {
+						action: 'sample_ajax_call',
+						_wpnonce: ajax_object._wpnonce,
+					},
+					success: function (data) {
+						alert( data );
+					},
+				}
+			)
+		}
+	)
+})( jQuery );
--- retreat-ajaxing-woo/retreat-ajaxing-woo.php
+++ retreat-ajaxing-woo/retreat-ajaxing-woo.php
@@ -2,9 +2,48 @@
 /**
  * Plugin Name: Ajaxing your ( WooCommerce ) website.
  */
-class Retreat_Ajaxing_Woo{
+class Retreat_Ajaxing_Woo {
 	public function __construct() {
+		add_action( 'init', array( $this, 'register_my_script' ) );
 
+		// Logged in users.
+		add_action( 'wp_ajax_sample_ajax_call', array( $this, 'sample_ajax_call' ), 10 );
+		add_action( 'wp_ajax_sample_ajax_call', 'wp_die', 20 );
+		// Logged out users.
+		add_action( 'wp_ajax_nopriv_sample_ajax_call', array( $this, 'nopriv_sample_ajax_call' ), 10 );
+		add_action( 'wp_ajax_nopriv_sample_ajax_call', 'wp_die', 20 );
+	}
+
+	public function register_my_script() {
+		wp_enqueue_script( 'my-ajax-script', plugins_url( '/js/my_query.js', __FILE__ ), array( 'jquery' ) );
+		wp_localize_script(
+			'my-ajax-script',
+			'ajax_object',
+			array(
+				'ajax_url' => admin_url( 'admin-ajax.php' ),
+				'_wpnonce' => wp_create_nonce( 'my-nonce' ),
+			)
+		);
+	}
+
+	public function sample_ajax_call() {
+		$nonce = $_REQUEST['_wpnonce'];
+		if ( ! wp_verify_nonce( $nonce, 'my-nonce' ) ) {
+			// This nonce is not valid.
+			die( 'Security check' );
+		} else {
+			echo 'logged in';
+		}
+	}
+
+	public function nopriv_sample_ajax_call() {
+		$nonce = $_REQUEST['_wpnonce'];
+		if ( ! wp_verify_nonce( $nonce, 'my-nonce' ) ) {
+			// This nonce is not valid.
+			die( 'Security check' );
+		} else {
+			echo 'not logged in';
+		}
 	}
 }
 
